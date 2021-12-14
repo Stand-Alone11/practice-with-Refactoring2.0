9f93ca56c9307c9b288be3e838055343a1d41ad3
MinSu Kim <ms001118@gmail.com>
2021-12-15 08:24:11 +0900

    refactor: add property 'volumeCredit' in data class Performance

--- src/main/kotlin/dto/Performance.kt
+++ src/main/kotlin/dto/Performance.kt
@@ -3,4 +3,5 @@ package dto
 data class Performance(val playId: String, var audience: Int) {
     lateinit var play: Play // play 추가
     var amount: Int = -1 // amount 추가
+    var volumeCredit: Int = -1 // volumeCredit 추가
 }
