5b50f8a9c0099ca1a560b1d005fb0b654ebd0a27
MinSu Kim <ms001118@gmail.com>
2021-12-17 08:24:33 +0900

    refactor: create TragedyCalculator

--- src/main/kotlin/TragedyCalculator.kt
+++ src/main/kotlin/TragedyCalculator.kt
@@ -1,4 +1,13 @@
-package dto
+import dto.Performance
+import dto.Play
 
-class TragedyCalculator {
+class TragedyCalculator(aPerformance: Performance, aPlay: Play): PerformanceCalculator(aPerformance, aPlay) {
+    override fun amount(): Int {
+        var result = 40000
+        if(performance.audience > 30) {
+            result += 1000 * (performance.audience - 30)
+        }
+        return result
+    }
+    override val amount = amount()
 }
