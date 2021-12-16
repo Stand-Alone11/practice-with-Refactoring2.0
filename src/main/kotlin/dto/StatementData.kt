0664be4ff47171b3ae9f39795c1ceefbd5a6fb67
MinSu Kim <ms001118@gmail.com>
2021-12-16 10:20:38 +0900

    refactor: override toString() in StatementData

--- src/main/kotlin/dto/StatementData.kt
+++ src/main/kotlin/dto/StatementData.kt
@@ -3,4 +3,8 @@ package dto
 data class StatementData(var totalAmount: Int = -1, var totalVolumeCredits: Int = -1) {
     lateinit var customer: String
     lateinit var performances: List<Performance>
+
+    override fun toString(): String {
+        return "StatementData(totalAmount=$totalAmount, totalVolumeCredits=$totalVolumeCredits, customer=$customer)"
+    }
 }
