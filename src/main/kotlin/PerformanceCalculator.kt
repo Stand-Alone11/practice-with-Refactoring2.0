5fae354e675469a9e884d3da6f6cfa4f8a96ed80
MinSu Kim <ms001118@gmail.com>
2021-12-17 08:45:54 +0900

    refator: return audience over 30 in volumeCredits()

--- src/main/kotlin/PerformanceCalculator.kt
+++ src/main/kotlin/PerformanceCalculator.kt
@@ -4,32 +4,11 @@ import kotlin.math.floor
 
 open class PerformanceCalculator(val performance: Performance, val play: Play) {
     open fun amount(): Int {
-        var result = 0
-        when(play.type) {
-            "tragedy" -> {
-                result = 40000;
-                if(performance.audience > 30) {
-                    result += 1000 * (performance.audience - 30)
-                }
-            }
-            "comedy" -> {
-                result = 30000
-                if(performance.audience > 20) {
-                    result += 10000 + 500 * (performance.audience - 20)
-                }
-                result += 300 * performance.audience
-            }
-            else -> throw Exception("알 수 없는 장르: ${play.type}")
-        }
-        return result
+        throw Exception("하위 클래스에서 처리해야함")
     }
 
-    // slide volumeCredits() from InvoiceMaker.createStatementData()
     open fun volumeCredits(): Int {
-        var result = 0
-        result += Math.max(performance.audience - 30 , 0)
-        if("comedy" == play.type) result += floor((performance.audience / 5).toDouble()).toInt()
-        return result
+        return Math.max(performance.audience - 30 , 0)
     }
 
     open val amount = amount()
